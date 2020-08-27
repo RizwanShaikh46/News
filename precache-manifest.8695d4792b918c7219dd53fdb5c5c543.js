ea9ef80c2ffda912d9b6bee700602508a67bf6be
RizwanShaikh46 <iamrizwansk@gmail.com>
2020-08-27 16:22:37 +0530

    Updates

---
+++ precache-manifest.8695d4792b918c7219dd53fdb5c5c543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a21dd899fec87d09db88e6f441caa0f",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "b98ce77aa2c965010398",
+    "url": "/News/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "00683be41b7436ac92d2",
+    "url": "/News/static/css/main.9b153fc0.chunk.css"
+  },
+  {
+    "revision": "b98ce77aa2c965010398",
+    "url": "/News/static/js/2.07629a3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/News/static/js/2.07629a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00683be41b7436ac92d2",
+    "url": "/News/static/js/main.e725b17b.chunk.js"
+  },
+  {
+    "revision": "77145793146b8a7ce463",
+    "url": "/News/static/js/runtime-main.777a6c18.js"
+  }
+]);
